5845d0750b636c3311a262a1a5617d5845bd043e
insanegrv <insanegrv@gmail.com>
2015-11-03 01:51:36 +0300

    The Beginning

---
+++ Gruntfile.js
@@ -0,0 +1,127 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+
+    copy: {
+      build: {
+        files: [{
+          expand: true,
+          cwd: "src",
+          src: [
+            "img/**",
+            "index.html",
+            "form.html"
+          ],
+          dest: "build"
+        }]
+      }
+    },
+
+    clean: {
+      build: ["build"]
+    },
+
+    sass: {
+      style: {
+        files: {
+          "build/css/style.css": ["src/sass/style.scss"]
+        }
+      }
+    },
+
+    cmq: {
+      style: {
+        files: {
+          "build/css/style.css" : ["build/css/style.css"]
+        }
+      }
+    },
+
+    postcss: {
+      options: {
+        processors: [
+          require("autoprefixer")({browsers: "last 2 versions"})
+        ]
+      },
+      style: {
+        src: "build/css/*.css"
+      }
+    },
+
+    cssmin: {
+      options: {
+        keepSpecialComments: 0,
+        report: "gzip"
+      },
+      style: {
+        files: {
+          "build/css/style.min.css" : ["build/css/style.css"]
+        }
+      }
+    },
+
+    concat: {
+        options: {
+          separator: ';',
+        },
+        dist: {
+          src: ['src/js/tap.min.js', 'src/js/mustache.min.js', 'src/js/burger.js', 'src/js/range.js', 'src/js/addtraveller.js', 'src/js/senddata.js'],
+          dest: 'build/js/script.js',
+        },
+      },
+
+    minified : {
+      files: {
+        src: [
+        'build/js/*.js'
+        ],
+        dest: 'build/js/'
+      },
+      options: {
+        sourcemap: false,
+        allinone: true,
+        dest_filename: "script.min.js"
+      }
+    },
+
+    imagemin: {
+      images: {
+        options: {
+          optimizationLevel: 3
+        },
+        files: [{
+          expand: true,
+          src: ["build/img/**/*.{png, jpg, gif, svg}"]
+        }]
+      }
+    },
+
+    watch: {
+      style: {
+        files: ['sass/**/*.scss'],
+        tasks: ['sass', 'postcss'],
+        options: {
+          spawn: false,
+          livereload: true
+        }
+      }
+    }
+  });
+
+  grunt.registerTask("build", [
+    "clean",
+    "copy",
+    "sass",
+    "cmq",
+    "postcss",
+    "cssmin",
+    "concat",
+    "minified",
+    //"imagemin",
+    ]);
+
+};
---
+++ Readme.md
@@ -0,0 +1,3 @@
+--
+`indigoflow project`
+--
